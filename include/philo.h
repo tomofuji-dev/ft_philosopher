2ee9b26711a21fca46314deda4f551a5b723e2e7
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-11-17 10:10:59 +0900

    add structs to philo.h

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 08:38:54 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/11/17 08:39:31 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/11/17 10:09:33 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,51 @@
 # define PHILO_H
 
 # include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct timeval	t_timeval;
+typedef pthread_mutex_t	t_mutex;
+typedef pthread_t		t_pthread;
+
+typedef enum e_philo_status
+{
+	EAT;
+	SLEEP;
+	THINK;
+	TAKE_FORK;
+}	t_philo_status;
+
+typedef struct s_fork_order
+{
+	int	left;
+	int	right;
+}	t_fork_order;
+
+typedef struct s_philo
+{
+	int				index;
+	t_pthread		*pthread;
+	t_fork_order	fork_order;
+	t_philo_status	status;
+	int				n_eat;
+}	t_philo;
+
+typedef struct s_table
+{
+	t_philo	*philos;
+	t_mutex	*forks;
+}	t_table;
+
+typedef struct s_env
+{
+	int			n_philos;
+	t_timeval	start;
+	t_timeval	time_to_die;
+	t_timeval	time_to_eats;
+	t_timeval	time_to_sleep;
+	int			n_must_eat;
+	t_table		table;
+}	t_env;
 
 #endif

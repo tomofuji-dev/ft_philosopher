0020e76b9fd190bf5e8dbe07ef9c2d0575f312b9
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-11-19 13:53:11 +0900

    add comment to monitor

--- src/process/process_monitor.c
+++ src/process/process_monitor.c
@@ -6,20 +6,23 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 09:53:54 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/11/19 10:24:37 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:52:56 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "process.h"
 
-void	*process_monitor(t_monitor *monitor)
+void	*process_monitor(t_philo *monitor)
 {
 	t_env	*env;
 
 	env = monitor->env;
-	usleep(-1 * timestamp_ms(env->start) * 1000);
+	wait_until(env->start);
 	while (env->finish == false)
 	{
-		
+		// check now < philo->last_meal_time + env->time_to_die
+		// check n_already_eat > n_must_eat
+		// if check pass, change finish_mutex change to true
+		// if 
 	}
 }

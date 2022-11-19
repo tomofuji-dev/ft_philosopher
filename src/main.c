c0206a35a20765be9eee9690bc78a1fee09c2ab6
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-11-19 13:39:08 +0900

    comment main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 08:39:40 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/11/17 15:50:48 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/11/19 09:56:05 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int	main(int argc, char *argv[])
 	bzero(&env, sizeof(t_env));
 	if (init(&env, argc, argv) == false)
 		return (1);
+	// close_all();
 	printf("n_philos: %d\n", env.n_philos);
 	print_timeval("time_to_die: ", env.time_to_die);
 	print_timeval("time_to_eat: ", env.time_to_eat);

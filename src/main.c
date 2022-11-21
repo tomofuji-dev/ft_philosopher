111fe7f6e134953244c69850cf7d5cce23914ae2
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-11-21 14:53:16 +0900

    update main.c

--- src/main.c
+++ src/main.c
@@ -6,19 +6,15 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 08:39:40 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/11/19 13:39:21 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/11/21 14:50:07 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include "init.h"
+#include "close.h"
 #include <stdio.h>
 #include <strings.h>
-
-static void	print_timeval(char *s, t_timeval t)
-{
-	printf("%s%ld.%06ld\n", s, (long)t.tv_sec, (long)t.tv_usec);
-}
 
 int	main(int argc, char *argv[])
 {
@@ -27,10 +23,5 @@ int	main(int argc, char *argv[])
 	bzero(&env, sizeof(t_env));
 	if (init(&env, argc, argv) == false)
 		return (1);
-	// close_all();
-	printf("n_philos: %d\n", env.n_philos);
-	print_timeval("time_to_die: ", env.time_to_die);
-	print_timeval("time_to_eat: ", env.time_to_eat);
-	print_timeval("time_to_sleep: ", env.time_to_sleep);
-	printf("n_must_eat: %d\n", env.n_must_eat);
+	close_all(&env);
 }
